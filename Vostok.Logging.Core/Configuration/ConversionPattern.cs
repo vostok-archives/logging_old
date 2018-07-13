e93faa4eaa50d3f917530e07fb921eb36447cd47
Mylov Vasily <mylov@skbkontur.ru>
2018-07-13 18:54:46 +0500

    ConversionPattern new logic.

--- Vostok.Logging.Core/Configuration/ConversionPattern.cs
+++ Vostok.Logging.Core/Configuration/ConversionPattern.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using JetBrains.Annotations;
+using Vostok.Commons;
 using Vostok.Logging.Abstractions;
 
 namespace Vostok.Logging.Core.Configuration
 {
     public class ConversionPattern
     {
-        public string PatternStr { get; }
-
         public static ConversionPattern FromString([CanBeNull] string patternStr)
         {
-            return new ConversionPattern(patternStr, true);
+            return new ConversionPattern(patternStr);
         }
 
         public static bool TryParse([CanBeNull] string patternStr, out ConversionPattern result)
@@ -27,62 +27,102 @@ namespace Vostok.Logging.Core.Configuration
             return true;
         }
 
-        public static ConversionPattern Default => new ConversionPattern(string.Join(string.Empty, patternKeys.Keys.Select(k => $"{{{k}}}")), false);
+        public static ConversionPattern Default = new ConversionPattern(string.Join(string.Empty, patternKeys.Keys.Select(k => $"{{{k}}}")));
+
+
+        private static string GetComponentValue(EventComponentsType type, LogEvent @event, string propertyName)
+        {
+            switch (type)
+            {
+                case EventComponentsType.DateTime:
+                    return @event.Timestamp.ToString("HH:mm:ss zzz");
+
+                case EventComponentsType.Level:
+                    return @event.Level.ToString();
+
+                case EventComponentsType.Prefix:
+                    var prefixProperty = GetPropertyOrNull(@event, prefixPropertyName);
+                    return prefixProperty is IReadOnlyList<string> prefixes ? string.Join(" ", prefixes.Select(p => $"[{p.ToString()}]")): null;
+
+                case EventComponentsType.Message:
+                    return @event.MessageTemplate;
+
+                case EventComponentsType.Exception:
+                    return @event.Exception?.ToString();
+
+                case EventComponentsType.Property:
+                    return GetPropertyOrNull(@event, propertyName)?.ToString();
+
+                case EventComponentsType.Properties:
+                    return @event.Properties == null ? null : $"[properties: {string.Join(", ", @event.Properties.Select(p => $"{p.Key} = {p.Value.ToString()}"))}]";
+
+                case EventComponentsType.NewLine:
+                    return Environment.NewLine;
+
+                default:
+                    return null;
+            }
+        }
 
         public string Format([NotNull] LogEvent @event)
         {
-            object prefixProperty = null;
-            @event.Properties?.TryGetValue(prefixPropertyName, out prefixProperty);
-            var prefixes = prefixProperty is IReadOnlyList<string> array ? array.ToArray() : null;
-            var message = LogEventFormatter.FormatMessage(@event.MessageTemplate, @event.Properties);
-            var properties = @event.Properties != null
-                ? string.Join(", ", @event.Properties.Values
-                    .Select(p => (p as IFormattable)?.ToString(null, CultureInfo.InvariantCulture) ?? p.ToString()))
-                : null;
+            var builder = new StringBuilder(parts[0].type == EventComponentsType.StringStart ? parts[0].partSuffix : null);
 
-            var timestampStr = @event.Timestamp.ToString("HH:mm:ss zzz");
-            var levelStr = @event.Level;
-            var prefixStr = prefixes == null ? null : string.Join(" ", prefixes.Select(p => $"[{p}]"));
-            var messageStr = string.IsNullOrEmpty(message) ? null : message;
-            var exceptionStr = @event.Exception;
-            var newLine = Environment.NewLine;
-            var propertiesStr = string.IsNullOrEmpty(properties) ? null : properties;
+            for (var i = 0; i < parts.Length; i++)
+            {
+                var componentType = parts[i].type;
+                var postStr = parts[i].partSuffix;
+                var value = GetComponentValue(componentType, @event, parts[i].propertyName);
+                if (!string.IsNullOrEmpty(value))
+                {
+                    builder.Append(value);
+                    builder.Append(postStr);
+                }
+            }
 
-            var formattedLine = string.Format(formatString, timestampStr, levelStr, prefixStr, messageStr, exceptionStr, newLine, propertiesStr);
-
-            if (@event.Properties == null)
-                return formattedLine;
-
-            return Regex.Replace(formattedLine, singlePropertyPattern, 
-                m => @event.Properties.TryGetValue(m.Groups[1].Value, out var value)
-                    ? value.ToString() 
-                    : m.Value);
+            return builder.ToString();
         }
 
-        private ConversionPattern([CanBeNull] string patternStr, bool replaceKeys)
+        private ConversionPattern(string patternStr)
         {
-            PatternStr = patternStr;
-            patternStr = string.IsNullOrEmpty(patternStr)
-                ? string.Empty
-                : patternStr;
+            var anyKey = string.Join("|", patternKeys.Values);
+            var matches = Regex.Matches(patternStr, $"(?:^?%(?:{anyKey})|^)((?:[^%]|%(?!{anyKey}))*)", RegexOptions.IgnoreCase);
+            parts = new (EventComponentsType type, string propertyName, string partSuffix)[matches.Count];
 
-            formatString = patternStr;
-
-            if (replaceKeys)
+            for (var i = 0; i < matches.Count; i++)
             {
-                foreach (var key in patternKeys.Keys)
-                {
-                    var value = patternKeys[key];
-                    formatString = Regex.Replace(formatString, value, _ => $"{{{key}}}");
-                }
-
-                formatString = Regex.Replace(formatString, allPropertiesPattern, _ => $"{{{patternKeys.Count}}}");
+                var match = matches[i];
+                parts[i] = TransformMatch(match);
             }
+        }
+
+        private (EventComponentsType type, string propertyName, string partSuffix) TransformMatch(Match match)
+        {
+            var value = match.Value;
+            var propertyName = match.Groups[1].Value;
+            var suffix = match.Groups[2].Value;
+
+            if (!value.StartsWith("%"))
+                return (EventComponentsType.StringStart, null, suffix);
+
+            foreach (var key in patternKeys.Keys.Where(k => k != EventComponentsType.Property))
+            {
+                var pattern = $"%{patternKeys[key]}";
+                if (value.StartsWith(pattern, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    if (!string.IsNullOrEmpty(propertyName))
+                        return (EventComponentsType.Property, propertyName, suffix);
+
+                    return (key, null, suffix);
+                }
+            }
+
+            return (EventComponentsType.StringStart, null, value);
         }
 
         public override int GetHashCode()
         {
-            return formatString.ToLower().GetHashCode();
+            return 0;//formatString.ToLower().GetHashCode();
         }
 
         public override bool Equals(object obj)
@@ -95,23 +135,46 @@ namespace Vostok.Logging.Core.Configuration
             if (other == null)
                 return false;
 
-            return formatString.Equals(other.formatString, StringComparison.CurrentCultureIgnoreCase);
+            return true;//formatString.Equals(other.formatString, StringComparison.CurrentCultureIgnoreCase);
         }
 
-        private readonly string formatString;
-
-        private static readonly Dictionary<int, string> patternKeys = new Dictionary<int, string>
+        private static object GetPropertyOrNull(LogEvent @event, string propertyName)
         {
-            {0, "%(?:d|D)" },
-            {1, "%(?:l|L)" },
-            {2, "%(?:x|X)" },
-            {3, "%(?:m|M)" },
-            {4, "%(?:e|E)" },
-            {5, "%(?:n|N)" }
+            if (@event.Properties == null)
+                return null;
+
+            return @event.Properties.TryGetValue(propertyName, out var property)
+                ? property
+                : null;
+        }
+
+        private readonly (EventComponentsType type, string propertyName, string partSuffix)[] parts;
+
+        private static readonly Dictionary<EventComponentsType, string> patternKeys = new Dictionary<EventComponentsType, string>
+        {
+            { EventComponentsType.DateTime, "d" },
+            { EventComponentsType.Level, "l" },
+            { EventComponentsType.Prefix, "x" },
+            { EventComponentsType.Message, "m" },
+            { EventComponentsType.Exception, "e" },
+            { EventComponentsType.Property, @"p\((\w*)\)" },
+            { EventComponentsType.Properties, "p" },
+            { EventComponentsType.NewLine, "n" }
         };
 
-        private const string singlePropertyPattern = "%(?:p|P)\\((\\w*)\\)";
-        private const string allPropertiesPattern = "%(?:p|P)(?!\\()";
         private const string prefixPropertyName = "prefix";
+
+        private enum EventComponentsType
+        {
+            StringStart,
+            DateTime,
+            Level,
+            Prefix,
+            Message,
+            Exception,
+            Properties,
+            Property,
+            NewLine
+        }
     }
 }
