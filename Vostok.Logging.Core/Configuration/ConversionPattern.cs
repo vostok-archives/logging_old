0d7e8f91bd4d29bb57cb8ff216f634f0ae939d34
Mylov Vasily <mylov@skbkontur.ru>
2018-07-16 13:38:32 +0500

    ConversionPattern refactoring (part1).

--- Vostok.Logging.Core/Configuration/ConversionPattern.cs
+++ Vostok.Logging.Core/Configuration/ConversionPattern.cs
@@ -10,6 +10,31 @@ namespace Vostok.Logging.Core.Configuration
 {
     public class ConversionPattern
     {
+        static ConversionPattern()
+        {
+            patternKeys = new Dictionary<PatternPartType, string>
+            {
+                { PatternPartType.DateTime, "d" },
+                { PatternPartType.Level, "l" },
+                { PatternPartType.Prefix, "x" },
+                { PatternPartType.Message, "m" },
+                { PatternPartType.Exception, "e" },
+                { PatternPartType.Property, @"p\((\w*)\)" },
+                { PatternPartType.Properties, "p" },
+                { PatternPartType.NewLine, "n" }
+            };
+
+            var anyKeyRegex = string.Join("|", patternKeys.Values);
+
+            regexPattern = $"(?:^?%(?:{anyKeyRegex})|^)((?:[^%]|%(?!{anyKeyRegex}))*)";
+
+            Default = new ConversionPattern("%d %l %x %m %e %n");
+        }
+
+        public static ConversionPattern Default { get; }
+
+        public string PatternStr { get; }
+
         public static ConversionPattern FromString([CanBeNull] string patternStr)
         {
             return new ConversionPattern(patternStr);
@@ -25,49 +50,13 @@ namespace Vostok.Logging.Core.Configuration
             return true;
         }
 
-        public static ConversionPattern Default = new ConversionPattern(string.Join(string.Empty, patternKeys.Keys.Select(k => $"{{{k}}}")));
-
-        private static string GetComponentValue(TemplateComponentPart part, LogEvent @event)
-        {
-            switch (part.Type)
-            {
-                case FormatPatternType.DateTime:
-                    return @event.Timestamp.ToString("HH:mm:ss zzz");
-
-                case FormatPatternType.Level:
-                    return @event.Level.ToString();
-
-                case FormatPatternType.Prefix:
-                    var prefixProperty = GetPropertyOrNull(@event, prefixPropertyName);
-                    return prefixProperty is IReadOnlyList<string> prefixes ? string.Join(" ", prefixes.Select(p => $"[{p.ToString()}]")): null;
-
-                case FormatPatternType.Message:
-                    return @event.MessageTemplate;
-
-                case FormatPatternType.Exception:
-                    return @event.Exception?.ToString();
-
-                case FormatPatternType.Property:
-                    return GetPropertyOrNull(@event, part.PropertyName)?.ToString();
-
-                case FormatPatternType.Properties:
-                    return @event.Properties == null ? null : $"[properties: {string.Join(", ", @event.Properties.Select(p => $"{p.Key} = {p.Value.ToString()}"))}]";
-
-                case FormatPatternType.NewLine:
-                    return Environment.NewLine;
-
-                default:
-                    return null;
-            }
-        }
-
         public string Format([NotNull] LogEvent @event)
         {
-            var builder = new StringBuilder(parts[0].Type == FormatPatternType.StringStart ? parts[0].PartSuffix : null);
+            var builder = new StringBuilder(patternParts[0].Type == PatternPartType.StringStart ? patternParts[0].PartSuffix : null);
 
-            foreach (var part in parts)
+            foreach (var part in patternParts)
             {
-                var value = GetComponentValue(part, @event);
+                var value = GetPartValue(part, @event);
                 if (!string.IsNullOrEmpty(value))
                 {
                     builder.Append(value);
@@ -78,59 +67,41 @@ namespace Vostok.Logging.Core.Configuration
             return builder.ToString();
         }
 
-        private ConversionPattern(string patternStr)
+        private static string GetPartValue(PatternPart part, LogEvent @event)
         {
-            var anyKey = string.Join("|", patternKeys.Values);
-            var matches = Regex.Matches(patternStr, $"(?:^?%(?:{anyKey})|^)((?:[^%]|%(?!{anyKey}))*)", RegexOptions.IgnoreCase);
-            parts = new TemplateComponentPart[matches.Count];
-
-            for (var i = 0; i < matches.Count; i++)
+            switch (part.Type)
             {
-                var match = matches[i];
-                parts[i] = TransformMatch(match);
+                case PatternPartType.DateTime:
+                    return @event.Timestamp.ToString(dateTimeFormatString);
+
+                case PatternPartType.Level:
+                    return @event.Level.ToString();
+
+                case PatternPartType.Prefix:
+                    var prefixProperty = GetPropertyOrNull(@event, prefixPropertyName);
+                    return prefixProperty is IReadOnlyList<string> prefixes
+                        ? string.Join(" ", prefixes.Select(p => $"[{TryFormatProperty(p)}]"))
+                        : null;
+
+                case PatternPartType.Message:
+                    return @event.MessageTemplate;
+
+                case PatternPartType.Exception:
+                    return @event.Exception?.ToString();
+
+                case PatternPartType.Property:
+                    var property = GetPropertyOrNull(@event, part.PropertyName);
+                    return TryFormatProperty(property);
+
+                case PatternPartType.Properties:
+                    return TryFormatProperties(@event.Properties);
+
+                case PatternPartType.NewLine:
+                    return Environment.NewLine;
+
+                default:
+                    return null;
             }
-        }
-
-        private TemplateComponentPart TransformMatch(Match match)
-        {
-            var value = match.Value;
-            var propertyName = match.Groups[1].Value;
-            var suffix = match.Groups[2].Value;
-
-            if (!value.StartsWith("%"))
-                return new TemplateComponentPart(FormatPatternType.StringStart, null, suffix);
-
-            foreach (var key in patternKeys.Keys.Where(k => k != FormatPatternType.Property))
-            {
-                var pattern = $"%{patternKeys[key]}";
-                if (value.StartsWith(pattern, StringComparison.CurrentCultureIgnoreCase))
-                {
-                    if (!string.IsNullOrEmpty(propertyName))
-                        return new TemplateComponentPart(FormatPatternType.Property, propertyName, suffix);
-
-                    return new TemplateComponentPart(key, null, suffix);
-                }
-            }
-
-            return new TemplateComponentPart(FormatPatternType.StringStart, null, value);
-        }
-
-        public override int GetHashCode()
-        {
-            return 0;//formatString.ToLower().GetHashCode();
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as ConversionPattern);
-        }
-
-        private bool Equals(ConversionPattern other)
-        {
-            if (other == null)
-                return false;
-
-            return true;//formatString.Equals(other.formatString, StringComparison.CurrentCultureIgnoreCase);
         }
 
         private static object GetPropertyOrNull(LogEvent @event, string propertyName)
@@ -143,23 +114,94 @@ namespace Vostok.Logging.Core.Configuration
                 : null;
         }
 
-        private readonly TemplateComponentPart[] parts;
-
-        private static readonly Dictionary<FormatPatternType, string> patternKeys = new Dictionary<FormatPatternType, string>
+        private static string TryFormatProperty(object property)
         {
-            { FormatPatternType.DateTime, "d" },
-            { FormatPatternType.Level, "l" },
-            { FormatPatternType.Prefix, "x" },
-            { FormatPatternType.Message, "m" },
-            { FormatPatternType.Exception, "e" },
-            { FormatPatternType.Property, @"p\((\w*)\)" },
-            { FormatPatternType.Properties, "p" },
-            { FormatPatternType.NewLine, "n" }
-        };
+            if (property == null)
+                return null;
 
+            return property.ToString();
+        }
+
+        private static string TryFormatProperties(IReadOnlyDictionary<string, object> properties)
+        {
+            if (properties == null)
+                return null;
+
+            return $"[properties: {string.Join(", ", properties.Select(p => $"{p.Key} = {TryFormatProperty(p.Value)}"))}]";
+        }
+
+        private ConversionPattern(string patternStr)
+        {
+            if (string.IsNullOrEmpty(patternStr))
+            {
+                PatternStr = string.Empty;
+                patternParts = new PatternPart[0];
+                return;
+            }
+
+            PatternStr = patternStr;
+
+            var matches = Regex.Matches(patternStr, regexPattern, RegexOptions.IgnoreCase);
+            patternParts = new PatternPart[matches.Count];
+
+            for (var i = 0; i < matches.Count; i++)
+            {
+                var match = matches[i];
+                patternParts[i] = CreatePatternPart(match);
+            }
+        }
+
+        private static PatternPart CreatePatternPart(Match match)
+        {
+            var value = match.Value;
+            var propertyName = match.Groups[1].Value;
+            var suffix = match.Groups[2].Value;
+
+            if (!value.StartsWith("%"))
+                return new PatternPart(PatternPartType.StringStart, null, suffix);
+
+            foreach (var key in patternKeys.Keys.Where(k => k != PatternPartType.Property))
+            {
+                var pattern = $"%{patternKeys[key]}";
+                if (value.StartsWith(pattern, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    if (!string.IsNullOrEmpty(propertyName))
+                        return new PatternPart(PatternPartType.Property, propertyName, suffix);
+
+                    return new PatternPart(key, null, suffix);
+                }
+            }
+
+            return new PatternPart(PatternPartType.StringStart, null, value);
+        }
+
+        public override int GetHashCode()
+        {
+            return PatternStr.ToLower().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ConversionPattern);
+        }
+
+        private bool Equals(ConversionPattern other)
+        {
+            if (other == null)
+                return false;
+
+            return PatternStr.Equals(other.PatternStr, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        private readonly PatternPart[] patternParts;
+
+        private static readonly Dictionary<PatternPartType, string> patternKeys;
+        private static readonly string regexPattern;
+
+        private const string dateTimeFormatString = "HH:mm:ss zzz";
         private const string prefixPropertyName = "prefix";
 
-        private enum FormatPatternType
+        private enum PatternPartType
         {
             StringStart,
             DateTime,
@@ -172,13 +214,13 @@ namespace Vostok.Logging.Core.Configuration
             NewLine
         }
 
-        private class TemplateComponentPart
+        private class PatternPart
         {
-            public FormatPatternType Type { get; }
+            public PatternPartType Type { get; }
             public string PropertyName { get; }
             public string PartSuffix { get; }
 
-            public TemplateComponentPart(FormatPatternType type, string propertyName, string suffix)
+            public PatternPart(PatternPartType type, string propertyName, string suffix)
             {
                 Type = type;
                 PropertyName = propertyName;
